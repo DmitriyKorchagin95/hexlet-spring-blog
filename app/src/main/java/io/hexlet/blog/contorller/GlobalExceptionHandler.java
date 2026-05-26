fec5e62113536f7142743acddc425cffa949c591
Dmitriy Korchagin <dmitriy.korchagin95@icloud.com>
2026-05-26 15:31:27 +0300

    Stop extending ResponseEntityExceptionHandler
    Remove unused ResponseEntityExceptionHandler import and stop extending it in GlobalExceptionHandler. The handler now relies on custom @ExceptionHandler methods without inheriting Spring's ResponseEntityExceptionHandler behavior.

--- app/src/main/java/io/hexlet/blog/contorller/GlobalExceptionHandler.java
+++ app/src/main/java/io/hexlet/blog/contorller/GlobalExceptionHandler.java
@@ -6,10 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
